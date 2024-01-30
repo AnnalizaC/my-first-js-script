bae4e2eee932edcfe3590ffaf85f88c8d95ba7d0
Annaliza_C <aldcadiente@gmail.com>
2024-01-30 13:45:49 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// Write your JavaScript below this line
+console.log("Hellllllo There!");
+
+//First Variable
+var name = "Annaliza";
+var favoriteSnack = "candy";
+
+console.log(
+  "Hi, my name is " +
+    name +
+    " and I wish I had " +
+    favoriteSnack +
+    " right now."
+);
